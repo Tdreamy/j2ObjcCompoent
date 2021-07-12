8e38266998344eee7cc47207b8efca16d733f153
dfg <allen.zhao@dfgroup.pro>
2021-07-12 15:35:26 +0800

    添加子模块，项目配置

---
+++ J2OCCOmpoent/J2OCCOmpoent/J2OCCOmpoent.h
@@ -0,0 +1,18 @@
+//
+//  J2OCCOmpoent.h
+//  J2OCCOmpoent
+//
+//  Created by dfg on 2021/7/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for J2OCCOmpoent.
+FOUNDATION_EXPORT double J2OCCOmpoentVersionNumber;
+
+//! Project version string for J2OCCOmpoent.
+FOUNDATION_EXPORT const unsigned char J2OCCOmpoentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <J2OCCOmpoent/PublicHeader.h>
+
+
